11b367997723b48c3a019e2461eaee0f5f12c287
Pratik Ratnaparkhi <pratikr557@gmail.com>
2016-06-23 19:52:15 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,21 @@
+
+public class Pattern {
+
+	public static void main(String[] args) {
+
+		int row, column;
+		for (row = 1; row <= 5; row++) {
+			for (column = 1; column <= row; column++) {
+				System.out.print(column);
+			}
+			System.out.println("");
+		}
+		for (row = 0; row < 4; row++) {
+			for (column = 1; column <= 4 - row; column++) {
+				System.out.print(column);
+			}
+			System.out.println("");
+		}
+	}
+
+}
